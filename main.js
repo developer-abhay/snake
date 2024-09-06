2668faea8cdd59c428f64004b897f4c004dca7b1
Abhay <abhaysharma.developer@gmail.com>
2024-09-06 13:11:36 +0530

    Moving snake on key commands

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 import "./style.css";
 
 const board = document.getElementById("app");
-
 const rows = Array(20).fill("");
-const grid = Array(20)
+let grid = Array(20)
   .fill()
   .map(() => [...rows]);
 
-grid.forEach((item, rowIndex) => {
-  item.forEach((data, colIndex) => {
-    board.appendChild(createDiv(rowIndex, colIndex));
-  });
-});
+let snake = [
+  [10, 10],
+  [10, 9],
+  [10, 8],
+];
 
+let direction = "up";
+
+// Create cells
 function createDiv(rowIndex, colIndex) {
   const div = document.createElement("div");
   div.classList.add("cell");
@@ -21,6 +23,7 @@ function createDiv(rowIndex, colIndex) {
   return div;
 }
 
+// Create food
 function createFood() {
   const oldFood = document.querySelector(".food");
   if (oldFood) {
@@ -33,10 +36,96 @@ function createFood() {
 
   const food = document.createElement("div");
   food.classList.add("food");
-  console.log(food);
+
   div.appendChild(food);
 }
 
-createFood();
+// Render snake
+function renderSnake() {
+  snake.forEach((item, index) => {
+    const div = document.querySelector(
+      `[data-row='${item[0]}'][data-col='${item[1]}']`
+    );
+    const snakeCell = document.createElement("div");
+    snakeCell.classList.add("snake-cell");
+    if (index == 0) {
+      snakeCell.classList.add("snake-head");
+    }
 
-setInterval(createFood, 2000);
+    div.appendChild(snakeCell);
+  });
+}
+
+// Move snake
+function moveSnake() {
+  const copy = JSON.parse(JSON.stringify(snake)); //Deep copy of snake
+
+  if (direction == "up") {
+    copy.forEach((item, index) => {
+      if (index == 0) {
+        item[0] -= 1;
+      } else {
+        copy[index] = snake[index - 1];
+      }
+    });
+  } else if (direction == "down") {
+    copy.forEach((item, index) => {
+      if (index == 0) {
+        item[0] += 1;
+      } else {
+        copy[index] = snake[index - 1];
+      }
+    });
+  } else if (direction == "right") {
+    copy.forEach((item, index) => {
+      if (index == 0) {
+        item[1] += 1;
+      } else {
+        copy[index] = snake[index - 1];
+      }
+    });
+  } else {
+    copy.forEach((item, index) => {
+      if (index == 0) {
+        item[1] -= 1;
+      } else {
+        copy[index] = snake[index - 1];
+      }
+    });
+  }
+
+  snake = [...copy];
+  const cells = document.querySelectorAll(".cell");
+  cells.forEach((cell) => {
+    if (cell.children[0]?.classList.contains("snake-cell")) {
+      cell.innerHTML = "";
+    }
+  });
+  renderSnake();
+  console.log(snake);
+}
+
+// Arrow keys event listeners
+window.addEventListener("keydown", (e) => {
+  if (e.key == "ArrowUp") {
+    direction = "up";
+  } else if (e.key == "ArrowDown") {
+    direction = "down";
+  } else if (e.key == "ArrowLeft") {
+    direction = "left";
+  } else if (e.key == "ArrowRight") {
+    direction = "right";
+  }
+});
+
+// Rendering grid
+grid.forEach((item, rowIndex) => {
+  item.forEach((data, colIndex) => {
+    board.appendChild(createDiv(rowIndex, colIndex));
+  });
+});
+
+// createFood();
+// setInterval(createFood, 2000);
+renderSnake();
+setInterval(moveSnake, 1000);
